2790bafe27d84ee67a63f9412e4c8861d06ed848
mirman-adsina2016 <adsina2016@adsina2016.local>
2015-11-09 15:15:54 -0800

    4loor Done

--- 4_The-Ground-4loor/app.js
+++ 4_The-Ground-4loor/app.js
@@ -1,37 +1,19 @@
 
-//Use jQuery to collect the values from the fields and create a
-//JSON object containing them. Then, log the result to the console
-
-// Secondary Objective: Use the AJAX methods built into jQuery to send
-// The form data to http://x.mirman.org:9999. Write a callback to handle
-// the response.
-
-function sendData() {
-  $.ajax({
-    url: 'x.mirman.org:1031/contact',
+$("#contact-form").submit(function(event){
+  console.log(event);
+  event.preventDefault();
+  $.ajax ({
+    url: 'http://x.mirman.org:1031/ground4loor',
     type: 'POST',
     contentType: 'json',
     data: {
-      $(document).on("submit", function(event) {
-        event.preventDefault();
-        console.log( $("#firstname").serialize())
-        console.log( $("#lastname").serialize())
-        console.log( $("#email").serialize())
-        console.log( $("#message").serialize())
-      });
+        "firstName": $("#firstname").val(),
+        "lastName": $("#lastname").val(),
+        "email": $("#email").val(),
+        "message": $("#message").val()
+      }
     },
-    dataType: 'json'
-  });
-  alert("json posted!");
-};
-
-
-
-
-/*
-var userInfo = {
-first: $("#firstname").serialize(),
-last: $("#lastname").serialize(),
-email: $("#email").serialize(),
-message: $("#message").serialize()
-*/
+    function(res) {
+      console.log(response)
+    });
+});
